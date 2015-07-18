af58d95c4f1bfbd46b1044b08f7d8950701cee65
Charles Bai <charlesbai@Charless-MacBook-Air.local>
2015-07-18 15:30:49 -0400

    pictures load through URL

--- artcrawl/app/src/main/java/com/charlesbai/artcrawl/MainActivity.java
+++ artcrawl/app/src/main/java/com/charlesbai/artcrawl/MainActivity.java
@@ -5,14 +5,128 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the reference to the ImageView
+        mImageView = (ImageView) findViewById(R.id.image);
+
+        // You can set a temporary background here
+        //image.setImageResource(null);
+
+        // Start the DownloadImage task with the given url
+        new DownloadImage().execute("http://wallpaperswa.com/thumbnails/detail/20120415/paintings%20colorful%20leonid%20afremov%20artwork" +
+                "%201920x1080%20wallpaper_wallpaperswa.com_17.jpg");
     }
+
+    /**
+     * Simple functin to set a Drawable to the image View
+     * @param drawable
+     */
+    private void setImage(Drawable drawable)
+    {
+        mImageView.setBackgroundDrawable(drawable);
+    }
+
+    public class DownloadImage extends AsyncTask<String, Integer, Drawable> {
+
+        @Override
+        protected Drawable doInBackground(String... arg0) {
+            // This is done in a background thread
+            return downloadImage(arg0[0]);
+        }
+
+        /**
+         * Called after the image has been downloaded
+         * -> this calls a function on the main thread again
+         */
+        protected void onPostExecute(Drawable image)
+        {
+            setImage(image);
+        }
+
+
+        /**
+         * Actually download the Image from the _url
+         * @param _url
+         * @return
+         */
+        private Drawable downloadImage(String _url)
+        {
+            //Prepare to download image
+            URL url;
+            BufferedOutputStream out;
+            InputStream in;
+            BufferedInputStream buf;
+
+            //BufferedInputStream buf;
+            try {
+                url = new URL(_url);
+                in = url.openStream();
+
+            /*
+             * THIS IS NOT NEEDED
+             *
+             * YOU TRY TO CREATE AN ACTUAL IMAGE HERE, BY WRITING
+             * TO A NEW FILE
+             * YOU ONLY NEED TO READ THE INPUTSTREAM
+             * AND CONVERT THAT TO A BITMAP
+            out = new BufferedOutputStream(new FileOutputStream("testImage.jpg"));
+            int i;
+
+             while ((i = in.read()) != -1) {
+                 out.write(i);
+             }
+             out.close();
+             in.close();
+             */
+
+                // Read the inputstream
+                buf = new BufferedInputStream(in);
+
+                // Convert the BufferedInputStream to a Bitmap
+                Bitmap bMap = BitmapFactory.decodeStream(buf);
+                if (in != null) {
+                    in.close();
+                }
+                if (buf != null) {
+                    buf.close();
+                }
+
+                return new BitmapDrawable(bMap);
+
+            } catch (Exception e) {
+                Log.e("Error reading file", e.toString());
+            }
+
+            return null;
+        }
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
